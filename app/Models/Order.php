77f369fce4d0de27b3edb4ea4850680cab99708c
LongDinhh <dinhlong.utt@gmail.com>
2021-05-31 09:32:00 +0700

    Update model Order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,5 +4,93 @@ namespace App\Models;
 
 class Order
 {
+    protected int $id;
 
+    protected int $productId;
+
+    protected int $grossPrince;
+
+    protected float $weightCoefficient;
+
+    protected float $dimensionCoefficient;
+
+    /**
+     * @param int $weightCoefficient
+     */
+    public function setWeightCoefficient(int $weightCoefficient): void
+    {
+        $this->weightCoefficient = $weightCoefficient;
+    }
+
+    /**
+     * @param int $dimensionCoefficient
+     */
+    public function setDimensionCoefficient(int $dimensionCoefficient): void
+    {
+        $this->dimensionCoefficient = $dimensionCoefficient;
+    }
+
+    /**
+     * @return int
+     */
+    public function getWeightCoefficient(): int
+    {
+        return $this->weightCoefficient;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDimensionCoefficient(): int
+    {
+        return $this->dimensionCoefficient;
+    }
+
+    /**
+     * @return int
+     */
+    public function getGrossPrince(): int
+    {
+        return $this->grossPrince;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getProductId(): int
+    {
+        return $this->productId;
+    }
+
+    /**
+     * @param int $grossPrince
+     */
+    public function setGrossPrince(int $grossPrince): void
+    {
+        $this->grossPrince = $grossPrince;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param int $productId
+     */
+    public function setProductId(int $productId): void
+    {
+        $this->productId = $productId;
+    }
 }
